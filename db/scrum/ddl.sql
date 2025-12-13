3d4fce21e1c79e97081d053bffd9db5e776fae0c
Dylan <dylanelias201931369@cunoc.edu.gt>
2025-12-13 00:57:58 -0600

    chore: follow members added

--- db/scrum/ddl.sql
+++ db/scrum/ddl.sql
@@ -18,3 +18,24 @@ CREATE TABLE
         closed_at TIMESTAMP,
         deleted_at TIMESTAMP
     );
+
+CREATE TABLE
+    project.users (
+        id UUID PRIMARY KEY,
+        first_name VARCHAR(100) NOT NULL,
+        last_name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        role VARCHAR(100) NOT NULL,
+        color VARCHAR(100) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP,
+        deleted_at TIMESTAMP
+    );
+
+CREATE TABLE
+    project.members (
+        project_id UUID REFERENCES project.projects (id),
+        user_id UUID REFERENCES project.users (id),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (project_id, user_id)
+    );
